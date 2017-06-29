c9be84520a8e5a79e37ecb16d36674fd0c48fbc8
Vj, Nuzen <venkatesh.jatla@gmail.com>
2017-06-29 13:21:29 -0600

    Atom theme is made default

--- init.el
+++ init.el
@@ -20,18 +20,19 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" default)))
+    ("08b8807d23c290c840bbb14614a83878529359eaba1805618b3be7d61b0b0a32" "d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" default)))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-done ((t (:foreground "PaleGreen" :weight normal :strike-through t))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "LightSalmon" :strike-through t)))))
 
 
 ;; Make blackboard as default theme
-(add-hook 'after-init-hook (lambda () (load-theme 'blackboard)))
+(add-hook 'after-init-hook (lambda () (load-theme 'atom-one-dark)))
 
 
 ;; Setting F8 to neotree
@@ -39,15 +40,23 @@
 
 
 ;; Enable highlight-symbol-mode for all modes that load text-mode, lisp-mode
-(add-hook 'text-mode-hook 'highlight-symbol-mode)
-(add-hook 'emacs-lisp-mode-hook 'highlight-symbol-mode)
+;;(add-hook 'text-mode-hook 'highlight-symbol-mode)
+;;(add-hook 'emacs-lisp-mode-hook 'highlight-symbol-mode)
 
 ;; Strike through for "done" items in emacs
 (setq org-fontify-done-headline t)
-(custom-set-faces
- '(org-done ((t (:foreground "PaleGreen"   
-                 :weight normal
-                 :strike-through t))))
- '(org-headline-done 
-            ((((class color) (min-colors 16) (background dark)) 
-               (:foreground "LightSalmon" :strike-through t)))))
+
+
+;; Store in tmp files
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+
+;; Switch on paren mode
+(show-paren-mode 1)
+
+;; Enable highlight-symbol-mode for all modes that load text-mode, lisp-mode
+(add-hook 'text-mode-hook 'highlight-symbol-mode)
+(add-hook 'emacs-lisp-mode-hook 'highlight-symbol-mode)
