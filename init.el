1e9fd9d628c916bb3677b3ebc03cbfe584d6633b
nuzen <venkatesh.jatla@hotmail.com>
2017-07-19 15:32:31 -0600

    Update init.el

--- init.el
+++ init.el
@@ -6,8 +6,17 @@
 ;; 4. highlight-symbol-mode
 ;; 5. Auto-Complete
 ;; 6. ESS --> This one is downloaded from distribution repo. (apt-get install ess)
+;; 7. To use spell check please install ispell package from repo.
+;; 8. go-mode, go-complete, go-autocomplete
 
 ;; Adding melpa repository
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/"))
@@ -20,10 +29,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-method (quote synctex))
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(TeX-view-program-list (quote (("Okular" "okular --unique %o#src:%n%b"))))
+ '(TeX-view-program-selection
+   (quote
+    ((engine-omega "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Okular")
+     (output-html "xdg-open"))))
  '(custom-safe-themes
    (quote
     ("08b8807d23c290c840bbb14614a83878529359eaba1805618b3be7d61b0b0a32" "d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" default)))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (markdown-preview-mode go go-autocomplete go-complete neotree evil auto-complete auto-complete-auctex atom-dark-theme atom-one-dark-theme auctex auctex-latexmk highlight-symbol))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -61,3 +83,20 @@
 
 ;; Adding auto-complete mode for text
 (add-hook 'text-mode-hook 'auto-complete-mode)
+
+;; Auto-flyspell mode for Latex
+(add-hook 'LaTeX-mode-hook
+	  '(flyspell-mode t)
+	  '(ac-flyspell-workaround t)
+	  )
+
+;; Adding Auctex to work with okular
+
+
+
+;; Python mode
+(add-hook 'python-mode-hook
+	  '(flyspell-mode t)
+	  '(auto-complete-mode t)
+	  )
+
