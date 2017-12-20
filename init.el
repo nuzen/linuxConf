b8b8835e74985d1df881bebe4db243105585e401
nuzen <venkatesh.jatla@hotmail.com>
2017-12-20 11:32:49 -0700

    Solarized color theme, save in .emacsSaves

--- init.el
+++ init.el
@@ -13,13 +13,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+ '(ansi-color-names-vector ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+ '(delete-selection-mode nil)
+ '(fci-rule-color "#3C3D37")
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors (quote (("#3C3D37" . 0) ("#679A01" . 20) ("#4BBEAE" . 30) ("#1DB4D0" . 50) ("#9A8F21" . 60) ("#A75B00" . 70) ("#F309DF" . 85) ("#3C3D37" . 100))))
  '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (php-mode markdown-mode auctex monokai-theme neotree))))
+ '(magit-diff-use-overlays nil)
+ '(package-selected-packages (quote (php-mode markdown-mode auctex monokai-theme neotree)))
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#F92672") (40 . "#CF4F1F") (60 . "#C26C0F") (80 . "#E6DB74") (100 . "#AB8C00") (120 . "#A18F00") (140 . "#989200") (160 . "#8E9500") (180 . "#A6E22E") (200 . "#729A1E") (220 . "#609C3C") (240 . "#4E9D5B") (260 . "#3C9F79") (280 . "#A1EFE4") (300 . "#299BA6") (320 . "#2896B5") (340 . "#2790C3") (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,8 +44,8 @@
 
 
 ;; ---- Monokai theme ---;
-(load-theme 'monokai)
-
+;;(load-theme 'monokai)
+(load-theme 'sanityinc-solarized-dark)
 
 ;; --- Title bar contains name of the buffer ---;;
 (setq frame-title-format "%b")
@@ -48,3 +58,7 @@
 
 ;; --- Adding line numbers by default ---;;
 (global-linum-mode 1)
+
+
+;; --- Saving all emacs files in ~/.emacsSaves ---
+(setq backup-directory-alist `(("." . "~/.emacsSaves")))
