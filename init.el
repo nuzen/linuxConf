0e3bae31a46634d2a09c130fd2b9428f1c2533f1
nuzen <venkatesh.jatla@hotmail.com>
2018-09-30 18:35:11 -0600

    Added webmode for editing php files

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
  '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
-    ("e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+    ("76dc63684249227d64634c8f62326f3d40cdc60039c2064174a7e7a7a88b1587" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
  '(delete-selection-mode nil)
  '(fci-rule-color "#3C3D37")
  '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
@@ -37,7 +37,7 @@
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (php-mode markdown-mode auctex monokai-theme neotree)))
+    (emmet-mode yasnippet company web-mode php-mode markdown-mode auctex monokai-theme neotree)))
  '(pos-tip-background-color "#FFFACE")
  '(pos-tip-foreground-color "#272822")
  '(vc-annotate-background nil)
@@ -71,8 +71,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-
-
 ;; ---- neotree ----;;
 (global-set-key [f8] 'neotree-toggle)
 
@@ -86,16 +84,58 @@
 (setq frame-title-format "%b")
 
 
-;; --- Auto load php-mode ---;;
-(autoload 'php-mode "php-mode" "Major mode for editing PHP code." t)
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
-(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
-
 ;; --- Adding line numbers by default ---;;
 (global-linum-mode 1)
 
 ;; --- Saving all emacs files in ~/.emacsSaves ---
 (setq backup-directory-alist `(("." . "~/.emacsSaves")))
 
-;; --- Setting font to consolas (Works better in windows) ---
-(set-face-attribute 'default nil :family "Consolas" :height 110)
+
+
+
+
+
+
+;; ------------------------------------
+;; ------ Web development settings ----
+;; ------------------------------------
+;; ------ Web link: https://fransiska.github.io/emacs/2017/08/21/web-development-in-emacs#org9198251
+
+;; Automatically load web-mode when opening web-related files
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+;; Set indentations
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)    
+
+;; Highlight columns
+(setq web-mode-enable-current-column-highlight t)
+(setq web-mode-enable-current-element-highlight t)
+
+;; Company settings
+(add-hook 'web-mode-hook 'company-mode)
+(defun my-web-mode-hook ()
+  (set (make-local-variable 'company-backends) '(company-css company-web-html company-yasnippet company-files))
+)
+
+;; Emmet settings
+(add-hook 'web-mode-hook  'emmet-mode) 
+(add-hook 'web-mode-before-auto-complete-hooks
+    '(lambda ()
+     (let ((web-mode-cur-language
+  	    (web-mode-language-at-pos)))
+               (if (string= web-mode-cur-language "php")
+    	   (yas-activate-extra-mode 'php-mode)
+      	 (yas-deactivate-extra-mode 'php-mode))
+               (if (string= web-mode-cur-language "css")
+    	   (setq emmet-use-css-transform t)
+      	 (setq emmet-use-css-transform nil)))))
