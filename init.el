b12983a958e05ca507100e042bd9bbbc3ba4cbf7
nuzen <venkatesh.jatla@hotmail.com>
2018-10-14 16:29:33 -0600

    Cleaned and updated

--- init.el
+++ init.el
@@ -1,69 +1,16 @@
-;; Adding MELPA repository
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;; -- Emacs custom set variables, automatically added by emacs -- ;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
- '(compilation-message-face (quote default))
- '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
-    ("76dc63684249227d64634c8f62326f3d40cdc60039c2064174a7e7a7a88b1587" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
- '(delete-selection-mode nil)
- '(fci-rule-color "#3C3D37")
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
- '(highlight-tail-colors
-   (quote
-    (("#3C3D37" . 0)
-     ("#679A01" . 20)
-     ("#4BBEAE" . 30)
-     ("#1DB4D0" . 50)
-     ("#9A8F21" . 60)
-     ("#A75B00" . 70)
-     ("#F309DF" . 85)
-     ("#3C3D37" . 100))))
+    ("bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(inhibit-startup-screen t)
- '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (xah-fly-keys solarized-theme emmet-mode yasnippet company web-mode php-mode markdown-mode auctex monokai-theme neotree)))
- '(pos-tip-background-color "#FFFACE")
- '(pos-tip-foreground-color "#272822")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#F92672")
-     (40 . "#CF4F1F")
-     (60 . "#C26C0F")
-     (80 . "#E6DB74")
-     (100 . "#AB8C00")
-     (120 . "#A18F00")
-     (140 . "#989200")
-     (160 . "#8E9500")
-     (180 . "#A6E22E")
-     (200 . "#729A1E")
-     (220 . "#609C3C")
-     (240 . "#4E9D5B")
-     (260 . "#3C9F79")
-     (280 . "#A1EFE4")
-     (300 . "#299BA6")
-     (320 . "#2896B5")
-     (340 . "#2790C3")
-     (360 . "#66D9EF"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+    (web-mode php-mode all-the-icons multi-web-mode neotree monokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -71,16 +18,23 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; -- To work better in windows --;;
+(set-face-attribute 'default nil :family "Consolas" :height 110)
 
-;; ---- Monokai theme ---;
-;;(load-theme 'monokai)
+
+;; --- Adding melpa packages -- ;;
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+;; ---- Theme ---;
+(load-theme 'monokai)
 ;;(load-theme 'sanityinc-solarized-dark)
 ;;(load-theme 'atom-dark)
-(load-theme 'solarized-dark)
+;;(load-theme 'solarized-dark)
 
 ;; --- Title bar contains name of the buffer ---;;
 (setq frame-title-format "%b")
-
 
 ;; --- Adding line numbers by default ---;;
 (global-linum-mode 1)
@@ -88,52 +42,15 @@
 ;; --- Saving all emacs files in ~/.emacsSaves ---
 (setq backup-directory-alist `(("." . "~/.emacsSaves")))
 
-
-
-
-
-
-
-;; ------------------------------------
-;; ------ Web development settings ----
-;; ------------------------------------
-;; ------ Web link: https://fransiska.github.io/emacs/2017/08/21/web-development-in-emacs#org9198251
-
-;; Automatically load web-mode when opening web-related files
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-
-
-
-(defun my-web-mode-hook ()
-  (set (make-local-variable 'company-backends) '(company-css company-web-html company-yasnippet company-files))
-)
-
-(add-hook 'web-mode-hook  'my-web-mode-hook)    
-
-;; Highlight columns
-(setq web-mode-enable-current-column-highlight t)
-(setq web-mode-enable-current-element-highlight t)
-
-;; Company settings
-(add-hook 'web-mode-hook 'company-mode)
-
-;; Emmet settings
-(add-hook 'web-mode-hook  'emmet-mode) 
-(add-hook 'web-mode-before-auto-complete-hooks
-    '(lambda ()
-     (let ((web-mode-cur-language
-  	    (web-mode-language-at-pos)))
-               (if (string= web-mode-cur-language "php")
-    	   (yas-activate-extra-mode 'php-mode)
-      	 (yas-deactivate-extra-mode 'php-mode))
-               (if (string= web-mode-cur-language "css")
-    	   (setq emmet-use-css-transform t)
-      	 (setq emmet-use-css-transform nil)))))
-
 ;; ---- neotree ----;;
 (global-set-key [f8] 'neotree-toggle)
 
+
+;; --- Multi web mode --- ;;
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'web-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
