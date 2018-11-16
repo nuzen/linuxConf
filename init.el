8a840ad7cab5e0f33305480e66c86c1707e43cd0
nuzen <venkatesh.jatla@hotmail.com>
2018-11-15 17:26:55 -0700

    Updated from red_fern

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-;; -- Emacs custom set variables, automatically added by emacs -- ;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -6,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (web-mode php-mode all-the-icons multi-web-mode neotree monokai-theme))))
+    (minimap auctex xah-fly-keys markdown-mode xah-find popup neotree monokai-theme async all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,20 +17,19 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; -- To work better in windows --;;
-(set-face-attribute 'default nil :family "Consolas" :height 110)
-
-
-;; --- Adding melpa packages -- ;;
-(package-initialize)
+;; --- Adding melpa --- ;;
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
 
-;; ---- Theme ---;
-(load-theme 'monokai)
-;;(load-theme 'sanityinc-solarized-dark)
-;;(load-theme 'atom-dark)
-;;(load-theme 'solarized-dark)
 
 ;; --- Title bar contains name of the buffer ---;;
 (setq frame-title-format "%b")
@@ -42,10 +40,16 @@
 ;; --- Saving all emacs files in ~/.emacsSaves ---
 (setq backup-directory-alist `(("." . "~/.emacsSaves")))
 
+
 ;; ---- neotree ----;;
-(global-set-key [f8] 'neotree-toggle)
+(global-set-key [f7] 'neotree-toggle)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(require 'all-the-icons)
 
+;; --- Theme --- ;;
+(load-theme 'monokai)
 
+;; --- Web development configuration --- ;;
 ;; --- Multi web mode --- ;;
 (require 'multi-web-mode)
 (setq mweb-default-major-mode 'web-mode)
@@ -54,3 +58,88 @@
                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
 (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
 (multi-web-global-mode 1)
+
+
+;; -- Auctex --- ;;
+(setq reftex-plug-into-AUCTeX t)
+
+
+
+
+
+
+
+
+
+;; --- Okular + Synctex --- ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Latex for Emacs
+;; 
+;; Dependencies: okular, texlive-full, auctex
+;;
+;; Okular setup: 
+;; 1.) Open Okular and go to...
+;; 2.) Settings -> Configure Okular -> Editor
+;; 3.) Set Editor to "Emacs client"
+;; 4.) Command should automatically set to: 
+;; emacsclient -a emacs --no-wait +%l %f
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; only start server for okular comms when in latex mode
+(add-hook 'LaTeX-mode-hook 'server-start)
+(setq TeX-PDF-mode t) ;; use pdflatex instead of latex
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Standard emacs/latex config
+;; http://emacswiki.org/emacs/AUCTeX
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+; enable auto-fill mode, nice for text
+(add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable synctex correlation
+(setq TeX-source-correlate-method 'synctex)
+;; Enable synctex generation. Even though the command shows
+;; as "latex" pdflatex is actually called
+(custom-set-variables '(LaTeX-command "latex -synctex=1") )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Use Okular as the pdf viewer. Build okular 
+;; command, so that Okular jumps to the current line 
+;; in the viewer.
+(setq TeX-view-program-selection
+ '((output-pdf "PDF Viewer")))
+(setq TeX-view-program-list
+ '(("PDF Viewer" "okular --unique %o#src:%n%b")))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;; --- Xah-fly-keys --- ;;;
+(require 'xah-fly-keys)
+
+(xah-fly-keys-set-layout "qwerty") ; required if you use qwerty
+
+;; possible layout values:
+;; "qwerty"
+;; "qwerty-abnt"
+;; "qwertz"
+;; "dvorak"
+;; "programer-dvorak"
+;; "colemak"
+;; "colemak-mod-dh"
+;; "workman"
+
+;; dvorak is the default
+
+(xah-fly-keys 1)
